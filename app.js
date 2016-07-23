54166e6fb5bc3fea0a5869e9cfe13813c84da565
Bogdan <info96532@gmail.com>
2016-07-23 14:31:10 +0300

    add html file, express, eslint, some styles

---
+++ app.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+app.set('port', 3000);
+app.set('path', __dirname);
+app.engine('html', require('ejs').renderFile);
+app.set('views engine', 'ejs');
+app.use(express.static(__dirname));
+
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/', function (req, res, next) {
+    res.setHeader('Content-Type', 'text/html');
+    res.render('main.html');
+    res.end();
+});
+app.use('/a', bodyParser.urlencoded({
+    extended: false
+}));
+app.post('/a', function (req, res, next) {
+    console.log(req.body.city);
+    res.end();
+});
+
+app.use(function (req, res) {
+    res.send(404, 'Page not found');
+});
+
---
+++ views/effects.js
@@ -0,0 +1,14 @@
+/*(function searchCity() {
+    var search = document.getElementById('search');
+    var results = document.getElementById('results');
+    var valueCity = document.getElementsByName('city')[0];
+
+    search.addEventListener('submit', function () {
+        var div = document.createElement('div');
+        div.innerHTML = valueCity.value;
+        valueCity.value = "";
+        results.appendChild(div);
+    });
+})();
+*/
+
